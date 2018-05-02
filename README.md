775bc52c5b0211f4bdd4b255a492f47bab019c7e
ligain <khokhlov86@gmail.com>
2018-05-02 21:18:31 +0300

    removed coins

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 # Price Formatter  
   
 Very simple tool to format number with spaces as thousands separator.
-Example: `3245.000000` -> `3 245.00`
+Example: `3245.000000` -> `3 245`
 # Usage as CLI
 ```bash
-$ python format_price.py -p 34534003459830459834059830945380453455.345
-Formatted price: 34 534 003 459 830 460 657 132 492 270 705 049 600.35
+$ python format_price.py -p 34534003459830459834059830945380453455.34
+Formatted price: 34 534 003 459 830 460 657 132 492 270 705 049 600
 ```
 # Usage as module
 ```python
 >>> from format_price import format_price
 >>> format_price('3245.000000')
-'3 245.00'
+'3 245'
 ```
 # Run tests
 ```bash
--- tests.py
+++ tests.py
@@ -19,13 +19,13 @@ class FormatPriceTest(unittest.TestCase):
         self.assertIsNone(format_price([]))
 
     def test_small_price(self):
-        self.assertEqual('0.50', format_price('.499999999999999'))
+        self.assertEqual('0', format_price('.499999999999999'))
         self.assertEqual('0', format_price('000000.32'))
 
     def test_long_price(self):
-        self.assertEqual('3 234 534 556 745.02', format_price('3234534556745.02'))
-        self.assertEqual('34 090.90', format_price('+34090.9000000000001'))
-        self.assertEqual('3 245.00', format_price('3245.000000'))
+        self.assertEqual('3 234 534 556 745', format_price('3234534556745.02'))
+        self.assertEqual('34 090', format_price('+34090.9000000000001'))
+        self.assertEqual('3 245', format_price('3245.000000'))
 
     def test_bad_string_argument(self):
         self.assertIsNone(format_price('sdf.90'))
